c13c1934dcc09fbdc27545cab5b7e1a0f3137a2f
imaizm <imaizm@gmail.com>
2016-03-04 20:42:11 +0900

    テストデータパターン追加

--- dmm-scraper.go
+++ dmm-scraper.go
@@ -13,4 +13,7 @@ func main() {
 	result = dmmCoJp.ScrapeFromItemDetail("http://www.dmm.co.jp/digital/videoa/-/detail/=/cid=h_150upsm00217/")
 	fmt.Println(result.Title)
 	fmt.Println(dmmCoJp.MarshalDmmCoJpItemToJSON(result))
+	result = dmmCoJp.ScrapeFromItemDetail("http://www.dmm.co.jp/digital/videoa/-/detail/=/cid=snth00001/")
+	fmt.Println(result.Title)
+	fmt.Println(dmmCoJp.MarshalDmmCoJpItemToJSON(result))
 }
